38bdcc9bc4dd00ddfb66eb440ae18779fff5ecbf
marcelo oliveira <marcelo.r.oliveira@ebradi.com.br>
2022-04-06 22:22:59 -0300

    basic-interm-advanced

--- KitchenShop/KitchenShop/urls.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.urls import path
-from urls import include
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('custom/', include('custom.urls'))
-]
